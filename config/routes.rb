baf095b2d18e591f0f932793bab5d0077c00660c
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2022-01-15 06:00:16 +0000

    [Add] 4-25 ルーティングの設定が完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  get 'homes/top'
-  root :to => "homes#top"
-  resources :users,only:[:index,:show,:edit]
-  resources :books,only:[:index,:show,:edit]
+  devise_for :users #deviseをしようする際にURLとして、usersを含むことを示す
+  root :to => "homes#top" # ルートパスをtopアクションと紐付け
+
+  #resourcesコマンドで自動生成するルーティングをonlyオプションで制限
+  resources :users,only:[:index,:create,:show,:edit,:update]
+  resources :books,only:[:index,:create,:show,:edit,:update,:destroy]
 
 end
