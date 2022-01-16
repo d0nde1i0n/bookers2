69bac57b6af248daf546e1d2a778e730398942f3
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2022-01-16 11:58:02 +0000

    [Add] 4-25 フラッシュメッセージの実装まで進行中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters,if: :devise_controller?
+
+  # サインアップ後の遷移先の指定
+  def after_sign_up_path_for(resource)
+    user_path(current_user.id)
+  end
+
+  # サインイン後の遷移先の指定
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
+
+  # サインアウト後の遷移先の指定
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    # emailというデータが保存可能となるように許可
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:email])
+  end
+
 end
+
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,42 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def show
+    @book_detail = Book.find(params[:id])
+    @book = Book.new
+    @user = current_user
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.delete
+    redirect_to books_path
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+   @book = Book.find(params[:id])
+   @book.update(book_params)
+   redirect_to book_path(@book)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
